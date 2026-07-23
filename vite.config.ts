c56df6f14c6d377c305a12b9a02f38a1729faec2
curiosityeducation <gogordon@iu.edu>
2026-07-23 14:33:56 -0400

    Update Vite base path to match the new gigi-mobile-app repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/gigi-app/',
+  base: '/gigi-mobile-app/',
   plugins: [react()],
   resolve: {
     alias: {
